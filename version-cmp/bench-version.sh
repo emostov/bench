ecb41d26c3d586ff776a847534af5718758983d9
emostov <32168567+emostov@users.noreply.github.com>
2021-02-25 15:02:03 -0800

    Update

--- version-cmp/bench-version.sh
+++ version-cmp/bench-version.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-echo "Starting up sidecar"
-(cd /home/zeke/substrate-api-sidecar \
-  && yarn start)
-
 echo -n "Enter sidecar version: "
 read SASVERSION
 echo "You entered sidecar version ${SASVERSION}"
@@ -12,7 +8,6 @@ echo -n "Enter blocks endpoint version (0 or 1): "
 read BLOCKVERSION
 echo "You entered block endpoint version ${BLOCKVERSION}"
 
-sleep 30
 echo ---
 echo Start benchmarking 2 request per sec
 siege --file=sidecar_bench_urls-v${BLOCKVERSION}.txt --delay=1 --log=siege-c2-sas-${SASVERSION}.log --concurrent=2 --reps=once
