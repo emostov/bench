a2a788884fcc913238d52ead29caeb6549db3be4
emostov <32168567+emostov@users.noreply.github.com>
2021-02-25 14:52:15 -0800

    Create script for comparison testing

---
+++ version-cmp/bench-version.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo starting up sidecar
+(cd /home/zeke/substrate-api-sidecar \
+  && yarn start)
+
+echo -n "Enter sidecar version: "
+read SASVERSION
+echo "You entered sidecar version ${SASVERSION}"
+
+echo -n "Enter blocks endpoint version (0 or 1): "
+read BLOCKVERSION
+echo "You entered block endpoint version ${BLOCKVERSION}"
+
+sleep 30
+echo ---
+echo Start benchmarking 2 request per sec
+siege --file=sidecar_bench_urls-v${BLOCKVERSION}.txt --delay=1 --log=siege-c2-sas-${SASVERSION}.log --concurrent=2 --reps=once
+
+
+echo Done with all testing
